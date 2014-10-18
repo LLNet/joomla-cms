a8b4aebe40ff7328706438eb36edd76d4c142ff3
Luis <luis@redweb.dk>
2014-10-18 10:03:05 +0200

    Code Style

--- templates/beez3/html/com_contact/contact/default_links.php
+++ templates/beez3/html/com_contact/contact/default_links.php
@@ -9,33 +9,40 @@
 
 defined('_JEXEC') or die;
 
-if ($this->params->get('presentation_style') == 'sliders'):?>
+?>
+
+<?php if ($this->params->get('presentation_style') == 'sliders') : ?>
 <div class="accordion-group">
 	<div class="accordion-heading">
 		<a class="accordion-toggle" data-toggle="collapse" data-parent="accordionContact" href="#display-links">
-		<?php echo JText::_('COM_CONTACT_LINKS');?>
+			<?php echo JText::_('COM_CONTACT_LINKS');?>
 		</a>
 	</div>
 	<div id="display-links" class="accordion-body collapse">
 		<div class="accordion-inner">
 <?php endif; ?>
-<?php if  ($this->params->get('presentation_style') == 'plain'):?>
-<?php echo '<h3>'. JText::_('JGLOBAL_ARTICLES').'</h3>'; ?>
+
+<?php if  ($this->params->get('presentation_style') == 'plain') : ?>
+	<h3><?php echo JText::_('JGLOBAL_ARTICLES'); ?></h3>
 <?php endif; ?>
 
 			<div class="contact-links">
 				<ul class="nav nav-list">
 					<?php
-					foreach (range('a', 'e') as $char) :// letters 'a' to 'e'
-						$link = $this->contact->params->get('link'.$char);
-						$label = $this->contact->params->get('link'.$char.'_name');
 
-						if (!$link) :
+					// Letters 'a' to 'e'
+					foreach (range('a', 'e') as $char)
+					{
+						$link = $this->contact->params->get('link' . $char);
+						$label = $this->contact->params->get('link' . $char . '_name');
+
+						if (!$link)
+						{
 							continue;
-						endif;
+						}
 
 						// Add 'http://' if not present
-						$link = (0 === strpos($link, 'http')) ? $link : 'http://'.$link;
+						$link = (0 === strpos($link, 'http')) ? $link : 'http://' . $link;
 
 						// If no label is present, take the link
 						$label = ($label) ? $label : $link;
@@ -45,11 +52,13 @@ if ($this->params->get('presentation_style') == 'sliders'):?>
 							    <?php echo $label; ?>
 							</a>
 						</li>
-					<?php endforeach; ?>
+						<?php
+					}
+					?>
 				</ul>
 			</div>
 
-<?php if ($this->params->get('presentation_style') == 'sliders'):?>
+<?php if ($this->params->get('presentation_style') == 'sliders') : ?>
 		</div>
 	</div>
 </div>
