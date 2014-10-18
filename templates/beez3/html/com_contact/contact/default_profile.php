f277344412ba37908db2184489d7147a00e21e24
Luis <luis@redweb.dk>
2014-10-18 10:05:20 +0200

    Code Style

--- templates/beez3/html/com_contact/contact/default_profile.php
+++ templates/beez3/html/com_contact/contact/default_profile.php
@@ -8,33 +8,38 @@
  */
 
 defined('_JEXEC') or die;
-?>
-<?php if (JPluginHelper::isEnabled('user', 'profile')) :
-	$fields = $this->item->profile->getFieldset('profile'); ?>
-<div class="contact-profile" id="users-profile-custom">
-	<dl class="dl-horizontal">
-	<?php foreach ($fields as $profile) :
-		if ($profile->value) :
-			echo '<dt>'.$profile->label.'</dt>';
-			$profile->text = htmlspecialchars($profile->value, ENT_COMPAT, 'UTF-8');
 
-			switch ($profile->id) :
-				case "profile_website" :
-					$v_http = substr($profile->profile_value, 0, 4);
+if (JPluginHelper::isEnabled('user', 'profile'))
+{
+	$fields = $this->item->profile->getFieldset('profile');
+	?>
+	<div class="contact-profile" id="users-profile-custom">
+		<dl class="dl-horizontal">
+			<?php
+			foreach ($fields as $profile)
+			{
+				if ($profile->value)
+				{
+					echo '<dt>' . $profile->label . '</dt>';
+					$profile->text = htmlspecialchars($profile->value, ENT_COMPAT, 'UTF-8');
 
-					if ($v_http == "http") :
-						echo '<dd><a href="'.$profile->text.'">'.$profile->text.'</a></dd>';
-					else :
-						echo '<dd><a href="http://'.$profile->text.'">'.$profile->text.'</a></dd>';
-					endif;
-					break;
+					if ($profile->id == 'profile_website')
+					{
+						if (substr($profile->value, 0, 4) != "http")
+						{
+							$profile->text = "http://" . $profile->text;
+						}
 
-				default:
-					echo '<dd>'.$profile->text.'</dd>';
-					break;
-			endswitch;
-		endif;
-	endforeach; ?>
-	</dl>
-</div>
-<?php endif; ?>
+						echo '<dd><a href="' . $profile->text . '">' . $profile->text . '</a></dd>';
+					}
+					else
+					{
+						echo '<dd>' . $profile->text . '</dd>';
+					}
+				}
+			}
+			?>
+		</dl>
+	</div>
+<?php
+}
